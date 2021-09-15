7f34dceb5b66415d39fc2682aff978637f424b79
Nosiphiwo Banzi <banzi@thedigitalacademy.co.za>
2021-09-15 11:41:45 +0200

    phone

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,12 @@ import { PhonebookModule } from './phonebook/phonebook.module';
 
 @Module({
 	imports: [
-		MongooseModule.forRoot("mongodb://localhost:27017/PHONEBOOK_DB", { autoCreate: true }),
+		MongooseModule.forRoot("mongodb+srv://NosiphiwoBanzi:<ShowMeTheM0ney>@cluster0.m8fy7.mongodb.net/phonebook-backend?retryWrites=true&w=majority", { autoCreate: true }),
 		PhonebookModule,
 	],
 	controllers: [],
 	providers: [],
 })
 export class AppModule {}
+
+//
